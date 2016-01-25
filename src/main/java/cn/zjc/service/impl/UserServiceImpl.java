2ee7f0060023f47f93892d84c387cc075418c1ec
zjcscut <15118850470@163.com>
2016-01-25 15:26:48 +0800

    first commit

--- src/main/java/cn/zjc/service/impl/UserServiceImpl.java
+++ src/main/java/cn/zjc/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package cn.zjc.service.impl;
 import cn.zjc.dao.UserDAO;
 import cn.zjc.entity.User;
 import cn.zjc.service.UserService;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +14,14 @@ import java.util.List;
  * 2016/1/21/14:42
  */
 @Service
-public class UserServiceImpl  implements UserService {
+public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserDAO userDAO;
 
     @Override
     public List<User> findUserByPhone(String phone) {
+        PageHelper.startPage(1, 2);
         List<User> list = userDAO.findByPhone(phone);
         return list;
     }
